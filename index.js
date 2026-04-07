c69c1cc6420d6635c58bf88829e09658b8fc960f
MrKacyk <89401209+MrKacyk@users.noreply.github.com>
2026-04-07 22:03:38 +0200

    Set JSON body size limit to 10mb
    Increased JSON request body size limit to 10mb.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json({ limit: '10mb' })); 
 // --- SZPIEG ZAPYTAŃ ---
 app.use((req, res, next) => {
     console.log(`[ OTRZYMANO STRZAŁ ] -> Metoda: ${req.method}, Cel: ${req.url}`);
